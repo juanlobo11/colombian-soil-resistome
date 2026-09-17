f4c66e0f83ce5cc716b20841b367f794b84c35c3
Juan Camilo Lobo Rúa <lobojuancamilo1@gmail.com>
2026-09-16 22:08:21 -0500

    Add Trimmomatic QC script for WGS data processing

---
+++ scripts/01_qc_trimming.sh
@@ -0,0 +1,31 @@
+﻿#!/bin/bash
+# ---------------------------------------------------------------------------
+# Script: 01_qc_trimming.sh
+# Description: Quality control and adapter trimming for WGS paired-end reads.
+# Tools: Trimmomatic
+# ---------------------------------------------------------------------------
+
+RAW_DATA="../data/raw"
+CLEAN_DATA="../data/clean"
+
+mkdir -p ${CLEAN_DATA}
+
+echo "Starting Quality Control with Trimmomatic..."
+
+for R1 in ${RAW_DATA}/*_1.fastq.gz; do
+    R2="${R1/_1.fastq.gz/_2.fastq.gz}"
+    SAMPLE=$(basename ${R1} _1.fastq.gz)
+    
+    echo "Processing sample: ${SAMPLE}"
+    
+    trimmomatic PE -threads 4 \
+        ${R1} ${R2} \
+        ${CLEAN_DATA}/${SAMPLE}_1_clean.fastq.gz ${CLEAN_DATA}/${SAMPLE}_1_unpaired.fastq.gz \
+        ${CLEAN_DATA}/${SAMPLE}_2_clean.fastq.gz ${CLEAN_DATA}/${SAMPLE}_2_unpaired.fastq.gz \
+        ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+        LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:50
+        
+    echo "Finished processing ${SAMPLE}"
+done
+
+echo "QC Pipeline completed successfully."
